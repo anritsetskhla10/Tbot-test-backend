7a4af69ff77d6d4c2b1fbbda25fd48b17fb80688
lukakhutsishvili <lukakhutsishvili777@gmail.com>
2025-02-17 16:25:57 +0400

    add .env

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ import cors from "cors";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
 import fs from "fs";
-import serviceAccount from "./serviceData.json";
+// import serviceAccount from "./serviceData.json";
+
+
 
 dotenv.config();
 
+const serviceAccount = {
+  type: process.env.type,
+  project_id:process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id:process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+}
+
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(fs.readFile(serviceAccount, "utf8"))),
+    credential: admin.credential.cert(serviceAccount),
   });
 const app = express();
 app.use(cors());
