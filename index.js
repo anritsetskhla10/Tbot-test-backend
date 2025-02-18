6cd35217200e3a71957012f0a1da99880c69d320
Anri Tsetskhlashvili <67234362+anritsetskhla10@users.noreply.github.com>
2025-02-18 16:18:48 +0400

    change for localhost

--- index.js
+++ index.js
@@ -53,4 +53,7 @@ app.post("/generate-token", async (req, res) => {
 });
 
 // Set server to listen on port 5000
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
